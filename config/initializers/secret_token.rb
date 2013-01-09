e992e9b82b6570df1d78ac1250afacecbd9225c8
Ian Morgan <ian@ruby-code.com>
2013-01-09 01:01:47 -0500

    base app, devise install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Genohair::Application.config.secret_token = '667f382b19066b2a4c60db9e634903c991090ecb322437114daae8ac90dae0953ff01843b336c13f49466a217cd995b045605814998d2a4e543187c1a1747953'
